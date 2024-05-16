ff47a5e5ed9d4e66fc2cca85f1bddaf908963be0
PΔBLØ ᄃΞ <pabloce@poscye.com>
2024-05-16 18:34:53 -0500

    fix: getBody

--- src/lib/getBody.ts
+++ src/lib/getBody.ts
@@ -1,11 +1,21 @@
 import type { RequestEvent } from '@sveltejs/kit';
 
-const getBody = (event: RequestEvent) => {
+const getBody = async (event: RequestEvent) => {
     const { request } = event;
-    if (request.body) {
-      return request.headers.get('content-type') === 'application/json' ? request.json() : request.text();
-    } else {
-      return null;
+    try {
+        const contentType = request.headers.get('content-type');
+
+        if (contentType === 'application/json') {
+            return await request.json();
+        }
+
+        return await request.text();
+    } catch (error) {
+        if (error instanceof SyntaxError) {
+            throw new Error('Failed to parse JSON request body');
+        } else {
+            throw new Error('Failed to read request body');
+        }
     }
 };
 
