a83def0a9cb359c6cbc8fd6ec8003c76286493bb
PΔBLØ ᄃΞ <pabloce@poscye.com>
2024-05-16 16:18:42 -0500

    fix: getbody

--- src/lib/getBody.ts
+++ src/lib/getBody.ts
@@ -1,12 +1,12 @@
 import type { RequestEvent } from '@sveltejs/kit';
 
-const getBody = async (event: RequestEvent) => {
-  const { request } = event;
-  if (request.body) {
-    return request.headers.get('content-type') === 'application/json' ? request.json() : request.text();
-  } else {
-    return null;
-  }
+const getBody = (event: RequestEvent) => {
+    const { request } = event;
+    if (request.body) {
+      return request.headers.get('content-type') === 'application/json' ? request.json() : request.text();
+    } else {
+      return null;
+    }
 };
 
 export { getBody };
